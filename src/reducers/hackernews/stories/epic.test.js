ee49050c18d2b2e78a2274f339ef3b04d02ef61c
Marceli Janowski <marceli@mjdesign.pl>
2018-04-19 14:40:51 +0100

    WIP: Update epics test

--- src/reducers/hackernews/stories/epic.test.js
+++ src/reducers/hackernews/stories/epic.test.js
@@ -1,14 +1,30 @@
 import { getHackernewsStoryEpic } from  './epic';
+import nock from 'nock';
 
-import * as service from '../../../service/hackernews/hackernews-service';
+import configureMockStore from 'redux-mock-store';
+import { createEpicMiddleware } from 'redux-observable';
+import { default as storiesEpic } from './epic';
+import { type, actions } from './action';
+
+const epicMiddleware = createEpicMiddleware(storiesEpic);
+const mockStore = configureMockStore([epicMiddleware]);
 
 describe('Hackernews stories epic', () => {
+  let store;
+
   beforeEach(() => {
-    service.get = jest.fn();
+    store = mockStore();
   });
+  afterEach(() => {
+    nock.cleanAll();
+    epicMiddleware.replaceEpic(storiesEpic);
+  });
+
   describe('getHackernewsStoryEpic', () => {
     it('should return success on request success', async () => {
-
+      store.dispatch({
+        type: type.GET_HACKERNEWS_STORIES_REQUEST
+      });
     });
     it('should return failure on request failure', async () => {
 
