215337aa7d7f1d16c0129b708d333309b8039639
Marceli Janowski <marceli@mjdesign.pl>
2018-06-03 11:08:54 +0100

    TASK 01 :  Create basic layout for list of hackernews (#2)

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
+import HackernewsList from '../containers/HackernewsList/HackernewsList';
+import "./home-page.scss";
+
 class HomePage extends Component {
   render() {
     return (
-      <div className="app">
-        <h1>Welcome</h1>
+      <div className="mdc-layout-grid">
+          <div className="mdc-layout-grid__cell">
+          <header className="mdc-toolbar">
+            <div className="mdc-toolbar__row">
+              <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
+                <a href="#" className="material-icons mdc-toolbar__menu-icon">menu</a>
+                <span className="mdc-toolbar__title">Hackernews demo page</span>
+              </section>
+            </div>
+            <HackernewsList type="topStory" />
+          </header>
+         
+        </div>
       </div>
     );
   }
